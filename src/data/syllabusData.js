57f92130963c84eb21b941f0707d74a50d0e8e26
DinukaD-24 <dinukailangakoon@gmail.com>
2026-06-09 02:33:11 +0530

    add slug and topics to the syllabus data structure; create syllabusCard component

--- src/data/syllabusData.js
+++ src/data/syllabusData.js
@@ -2,6 +2,7 @@ export const syllabusData = [
     {
         id: 1,
         name: "Local A/L",
+        slug: "local-al",
         grades: [
             {
                 id: 1,
@@ -10,10 +11,26 @@ export const syllabusData = [
                     {
                         id: 1,
                         name: "Combined Maths",
+                        topics: [
+                            {
+                                id: 1,
+                                name: "Trigonometry",
+                            },
+                            {
+                                id: 2,
+                                name: "Complex Numbers",
+                            },
+                        ],
                     },
                     {
                         id: 2,
                         name: "Physics",
+                        topics: [
+                            {
+                                id: 1,
+                                name: "Mechanics",
+                            },
+                        ],
                     },
                 ],
             },
