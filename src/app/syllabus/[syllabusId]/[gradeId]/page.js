f097946a1ffb5a8c1925d4330fa72afc85dd1607
DinukaD-24 <dinukailangakoon@gmail.com>
2026-06-09 03:39:59 +0530

    add syllabus pages for subject, grade, and syllabus IDs; update syllabus import path

---
+++ src/app/syllabus/[syllabusId]/[gradeId]/page.js
@@ -0,0 +1,7 @@
+export default function Page({ params }) {
+    return (
+        <div>
+            Grade: {params.gradeId}
+        </div>
+    );
+}
---
+++ src/app/syllabus/[syllabusId]/page.js
@@ -0,0 +1,7 @@
+export default function Page({ params }) {
+    return (
+        <div>
+            Syllabus: {params.syllabusId}
+        </div>
+    );
+}
