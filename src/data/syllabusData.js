6a0e92442bc46747556aedb08563a5e0a6e0da48
DinukaD-24 <dinukailangakoon@gmail.com>
2026-06-08 12:34:27 +0530

    Add syllabus datastructure for course information

---
+++ src/data/syllabusData.js
@@ -0,0 +1,22 @@
+export const syllabusData = [
+    {
+        id: 1,
+        name: "Local A/L",
+        grades: [
+            {
+                id: 1,
+                name: "Grade 12",
+                subjects: [
+                    {
+                        id: 1,
+                        name: "Combined Maths",
+                    },
+                    {
+                        id: 2,
+                        name: "Physics",
+                    },
+                ],
+            },
+        ],
+    },
+];
