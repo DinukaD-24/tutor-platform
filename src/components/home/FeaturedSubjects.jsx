d47f868d41959bb2c72471b7cfe7feea41c35296
DinukaD-24 <dinukailangakoon@gmail.com>
2026-06-24 20:52:52 +0530

    fix: update key prop in SubjectCard to ensure unique identification of subjects

--- src/components/home/FeaturedSubjects.jsx
+++ src/components/home/FeaturedSubjects.jsx
@@ -15,7 +15,7 @@ export default function FeaturedSubject() {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
                 {subjects.map((subject) => (
                     <SubjectCard
-                        key={subject.id}
+                        key={`${subject.syllabusSlug}-${subject.gradeSlug}-${subject.slug}`}
                         subject={subject}
                     />
                 ))}
