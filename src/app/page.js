345317180dfe323749de1e3d8aef05167f59ffc4
DinukaD-24 <dinukailangakoon@gmail.com>
2026-07-15 08:09:25 +0530

    feat: reorder component imports and maintain consistent formatting in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,19 @@
-import HeroSection from "@/components/home/HeroSection";
-import FeaturedSubjects from "@/components/home/FeaturedSubjects";
-import FeaturedTutors from "@/components/home/FeaturedTutors";
-import StatisticsSection from "@/components/home/StatisticsSection";
-import HowItWorksSection from "@/components/home/HowItWorksSection";
+import HeroSection          from "@/components/home/HeroSection";
+import StatisticsSection    from "@/components/home/StatisticsSection";
+import FeaturedSubjects     from "@/components/home/FeaturedSubjects";
+import FeaturedTutors       from "@/components/home/FeaturedTutors";
+import HowItWorksSection    from "@/components/home/HowItWorksSection";
+import TestimonialsSection  from "@/components/home/TestimonialsSection";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-background text-dark">
-      <HeroSection/>
-      <FeaturedSubjects/>
-      <FeaturedTutors/>
-      <StatisticsSection/>
-      <HowItWorksSection/>
-    </main>
-  );
+    return (
+        <main className="min-h-screen bg-background text-dark">
+            <HeroSection />
+            <StatisticsSection />
+            <FeaturedSubjects />
+            <FeaturedTutors />
+            <HowItWorksSection />
+            <TestimonialsSection />
+        </main>
+    );
 }
