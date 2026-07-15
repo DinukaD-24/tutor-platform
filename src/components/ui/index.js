3319b65c6d1f8ce58f8296ea25893da4d173873e
DinukaD-24 <dinukailangakoon@gmail.com>
2026-07-15 08:04:25 +0530

    feat: update index file to include FadeIn and Stagger components for improved UI exports

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,5 +1,7 @@
-export { default as SectionHeading } from "./SectionHeading";
-export { default as PageHeader }     from "./PageHeader";
-export { default as StatCard }       from "./StatCard";
-export { default as EmptyState }     from "./EmptyState";
-export { default as Badge }          from "./Badge";
+export { default as SectionHeading }     from "./SectionHeading";
+export { default as PageHeader }         from "./PageHeader";
+export { default as StatCard }           from "./StatCard";
+export { default as EmptyState }         from "./EmptyState";
+export { default as Badge }              from "./Badge";
+export { default as FadeIn }             from "./FadeIn";
+export { StaggerContainer, StaggerItem } from "./StaggerContainer";
