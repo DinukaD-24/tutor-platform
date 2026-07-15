d9e8bebb6ffe1a51200729041610b5c1c43fa2f4
DinukaD-24 <dinukailangakoon@gmail.com>
2026-07-15 07:46:40 +0530

    feat: add index file to export UI components for easier imports

---
+++ src/components/ui/index.js
@@ -0,0 +1,5 @@
+export { default as SectionHeading } from "./SectionHeading";
+export { default as PageHeader }     from "./PageHeader";
+export { default as StatCard }       from "./StatCard";
+export { default as EmptyState }     from "./EmptyState";
+export { default as Badge }          from "./Badge";
